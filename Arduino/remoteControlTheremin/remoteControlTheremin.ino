ed88fc125e426646795a73c857d9e7d7eceff3b2
Kars Alfrink <kars@leapfrog.nl>
2017-03-07 21:00:49 +0100

    Code for remote control theremin

---
+++ Arduino/remoteControlTheremin/remoteControlTheremin.ino
@@ -0,0 +1,169 @@
+#include <SPI.h>
+#include <WiFi101.h>
+#include <WiFiUdp.h>
+#include <OSCBundle.h>
+#include <OSCBoards.h>
+
+char ssid[] = "yourssid";           // your network SSID (name)
+char pass[] = "yourpass";           // your network password
+int status = WL_IDLE_STATUS;        // the WiFi radio's status
+
+unsigned int localPort = 0000;      // local port to listen on
+
+WiFiUDP Udp;
+
+const int speakerPin = 1;           // where the buzzer is attached
+const int buttonPin = 5;            // where the button is attached
+const int statusLedPin = LED_BUILTIN;
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(statusLedPin, OUTPUT);
+  digitalWrite(statusLedPin, LOW);
+  
+  // Initialize serial and wait for port to open:
+  Serial.begin(9600);
+    
+  // What this does: http://arduino.stackexchange.com/a/4557/31607
+  // Remove this if you want your sketch to run without a serial connection
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
+  // Check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // Don't continue:
+    while (true);
+  }
+
+  // Attempt to connect to WiFi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // Wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // You're connected now, so print out the data:
+  Serial.println();
+  Serial.println("You're connected to the network");
+  Serial.println();
+  printCurrentNet();
+  printWiFiData();
+
+  // Start listening for stuff over UDP
+  Udp.begin(localPort);
+  Serial.print("Listening on port: ");
+  Serial.println(localPort);
+  Serial.println();
+
+  // Switch on onboard LED so we know setup is done when running standalone
+  digitalWrite(statusLedPin, HIGH);
+}
+
+void loop() {
+  // Make an empty message to fill with the incoming data
+  OSCMessage msg;
+  int size;
+  // Reads and dispatches the incoming message
+  if( (size = Udp.parsePacket())>0) {
+    while(size--)
+      msg.fill(Udp.read());
+    
+    if(!msg.hasError())
+      msg.dispatch("/wek/outputs", dispatchAddress);
+  }
+
+  // Turn off the sound when the button is pressed
+  if (digitalRead(buttonPin) == LOW) {
+    noTone(speakerPin);
+  }
+}
+
+void dispatchAddress(OSCMessage &msg) {
+  // Returns true if the data in the first position is a float
+  if (msg.isFloat(0)) {
+    float val = msg.getFloat(0);
+    Serial.print("Message is float: ");
+    Serial.println(val);
+    Serial.println();
+
+    // Multiply float so we can map it to an int
+    // http://electronics.stackexchange.com/a/83462
+    float m = 3200.0f;
+    val = val * m;    
+
+    // Map value to range
+    // Output values are roughly the range of human hearing
+    // https://en.wikipedia.org/wiki/Audio_frequency
+    int mappedVal = map(val, 0, 1 * m, 16, 2048);
+    // Call function to set tone
+    playTone(mappedVal);
+  }
+}
+
+void playTone(int pitch) {
+  // Make noise
+  tone(speakerPin, pitch);
+}
+
+void printWiFiData() {
+  // Print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // Print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+  Serial.println();
+}
+
+void printCurrentNet() {
+  // Print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // Print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  Serial.print(bssid[5], HEX);
+  Serial.print(":");
+  Serial.print(bssid[4], HEX);
+  Serial.print(":");
+  Serial.print(bssid[3], HEX);
+  Serial.print(":");
+  Serial.print(bssid[2], HEX);
+  Serial.print(":");
+  Serial.print(bssid[1], HEX);
+  Serial.print(":");
+  Serial.println(bssid[0], HEX);
+
+  // Print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("Signal strength (RSSI): ");
+  Serial.println(rssi);
+
+  // Print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type: ");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
